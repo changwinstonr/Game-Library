21d1788d227fa5ccbfc557b7b3cb04414e57c06e
changwinstonr <chang.winstonr@gmail.com>
2016-05-02 16:59:48 -0700

    Adds: SearchView.

--- app/src/main/java/winston/gameLibrary/MainActivity.java
+++ app/src/main/java/winston/gameLibrary/MainActivity.java
@@ -2,6 +2,11 @@ package winston.gameLibrary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mStringList = new LinkedList<>();
+        mStringList.add("Read");
+        mStringList.add("Learn");
+        mStringList.add("Buy");
+
+        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mStringList);
+
+        ListView listName = (ListView) (findViewById(R.id.listView));
+        listName.setAdapter(mAdapter);
     }
 }
--- readme.md
+++ readme.md
@@ -1,2 +1,11 @@
+
+##Mission - Basic
+- Data persistance through a SQLite database (CRUD)
+- Searching and Filtering
+- Testing
+
+
 ##User Stories
 ![Home](screenshots/userStory.png)
+
+##Prototype & ERD
