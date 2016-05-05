ee27a801b169c98a235121a51f290ef17402a8fc
changwinstonr <chang.winstonr@gmail.com>
2016-05-05 12:17:19 -0700

    Saves: because I forgot to commit before my battery died. :persevere:

--- app/src/main/java/winston/gameLibrary/MainActivity.java
+++ app/src/main/java/winston/gameLibrary/MainActivity.java
@@ -12,6 +12,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -39,11 +40,17 @@ public class MainActivity extends AppCompatActivity {
 
     //begin intents
     @Override
-    protected void onNewIntent (Intent inten){
+    protected void onNewIntent (Intent intent){
         handleIntent(intent);
     }
 
     private void handleIntent(Intent intent){
+        if(Intent.ACTION_SEARCH.equals(intent.getAction())){
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Cursor cursor = GameSQLiteDatabaseHelper.getInstance(MainActivity.this).searchLibrary(query);
+            //second swapCursor on the intent
+            adapter.swapCursor(cursor);
+        }
 
     }
 
@@ -59,40 +66,55 @@ public class MainActivity extends AppCompatActivity {
 
         for (cursor.moveToFirst();!cursor.isAfterLast();cursor.moveToNext()){
             Games addGame = new Games();
-
-            addGame.setmName(cursor.getString(cursor.getColumnIndex("name")));
-            addGame.setmGenre(cursor.getString(cursor.getColumnIndex("genre")));
-            addGame.setmRelease(cursor.getString(cursor.getColumnIndex("release")));
-            addGame.setmBlurb(cursor.getString(cursor.getColumnIndex("blurb")));
+                addGame.setmName(cursor.getString(cursor.getColumnIndex("name")));
+                addGame.setmGenre(cursor.getString(cursor.getColumnIndex("genre")));
+                addGame.setmRelease(cursor.getString(cursor.getColumnIndex("release")));
+                addGame.setmBlurb(cursor.getString(cursor.getColumnIndex("blurb")));
 
             gamesDetail.add(addGame);
         }
+        //curse these cursoradapters
+        if (adapter == null){
+            adapter = new CursorAdapter(this, cursor, 1) {
+                @Override
+                public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
+                    //fatboyslim xml
+                    LayoutInflater layoutInflater = LayoutInflater.from(context);
+                    View view = layoutInflater.inflate(R.layout.activity_games, viewGroup, false);
+                    return view;
+                }
 
-    }
+                @Override
+                public void bindView(View view, Context context, Cursor cursor) {
+                    TextView gameName = (TextView) view.findViewById(R.id.game_name);
+                    String games = cursor.getString(cursor.getColumnIndex("name"));
+                    gameName.setText(games);
+                }
+            };
 
-    //curse these cursoradapters
-    if (adapter == null){
-        adapter = new CursorAdapter(this, cursor, 1) {
+            listView.setAdapter(adapter);
+        }else{
+            //Swap in a new Cursor, returning the old Cursor.
+            adapter.swapCursor(cursor);
+        }
+
+        //Listen for clicky, clicky, tappy, tappy onItem
+        listview.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()) {
             @Override
-            public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
-                //fatboyslim xml
-                LayoutInflater layoutInflater = LayoutInflater.from(context);
-                View view = layoutInflater.inflate(R.layout.activity_games, viewGroup, false);
-                return view;
-            }
+            public void onItemClick (AdapterView<?> parent, Vew view, int position, long id){
+                Cursor cursor = adapter.getCursor();
+                cursor.moveToPosition(position);
+                Intent intent = new Intent(MainActivity.this, );
+                intent.putExtra("name", cursor.getString(cursor.getColumnIndex(GameSQLiteDatabaseHelper.COL_NAME)));
+                intent.putExtra("genre", cursor.getString(cursor.getColumnIndex(GameSQLiteDatabaseHelper.COL_GENRE)));
+                intent.putExtra("release", cursor.getString(cursor.getColumnIndex(GameSQLiteDatabaseHelper.COL_RELEASE)));
+                intent.putExtra("blurb", cursor.getString(cursor.getColumnIndex(GameSQLiteDatabaseHelper.COL_BLURB)));
 
-            @Override
-            public void bindView(View view, Context context, Cursor cursor) {
-                TextView gameName = (TextView) view.findViewById(R.id.game_name);
-                String games = cursor.getString(cursor.getColumnIndex("name"));
-                gameName.setText(games);
+                startActivity(intent);
             }
-        };
+        });
 
-        listView.setAdapter(adapter);
-    }else{
-        //Swap in a new Cursor, returning the old Cursor.
-        adapter.swapCursor(cursor);
     }
 
 
