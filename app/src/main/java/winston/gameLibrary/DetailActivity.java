255d285644add9f303dd2d79dee4798c97bf969d
changwinstonr <chang.winstonr@gmail.com>
2016-05-05 16:47:50 -0700

    Fills: DetailActivity & activity_detail.

--- app/src/main/java/winston/gameLibrary/DetailActivity.java
+++ app/src/main/java/winston/gameLibrary/DetailActivity.java
@@ -1,7 +1,9 @@
 package winston.gameLibrary;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -11,6 +13,25 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        TextView name = (TextView) findViewById(R.id.game_name);
+        TextView genre = (TextView) findViewById(R.id.game_genre);
+        TextView release = (TextView) findViewById(R.id.game_release);
+        TextView blurb = (TextView) findViewById(R.id.game_blurb);
+
+        Intent intent = getIntent();
+
+        String mName = intent.getStringExtra("name");
+        String mGenre = intent.getStringExtra("genre");
+        String mRelease = intent.getStringExtra("release");
+        String mBlurb = intent.getStringExtra("blurb");
+
+        name.setText(mName);
+        genre.setText(mGenre);
+        release.setText(mRelease);
+        blurb.setText(mBlurb);
+
+
 
     }
 }
