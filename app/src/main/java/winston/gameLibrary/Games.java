c2a6a7484e582def61496a0a50e3df671dc4cd60
changwinstonr <chang.winstonr@gmail.com>
2016-05-05 05:23:07 -0700

    Adds: manual #OVERKILL seeding of db.

---
+++ app/src/main/java/winston/gameLibrary/Games.java
@@ -0,0 +1,51 @@
+package winston.gameLibrary;
+
+public class Games {
+    //per Graeme's advice. variables globalized
+    private String m_id;
+    private String mName;
+    private String mGenre;
+    private String mRelease;
+    private String mBlurb;
+
+    //generate get-set
+    public String getM_id() {
+        return m_id;
+    }
+
+    public void setM_id(String m_id) {
+        this.m_id = m_id;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public String getmGenre() {
+        return mGenre;
+    }
+
+    public void setmGenre(String mGenre) {
+        this.mGenre = mGenre;
+    }
+
+    public String getmRelease() {
+        return mRelease;
+    }
+
+    public void setmRelease(String mRelease) {
+        this.mRelease = mRelease;
+    }
+
+    public String getmBlurb() {
+        return mBlurb;
+    }
+
+    public void setmBlurb(String mBlurb) {
+        this.mBlurb = mBlurb;
+    }
+}
--- app/src/main/java/winston/gameLibrary/MainActivity.java
+++ app/src/main/java/winston/gameLibrary/MainActivity.java
@@ -1,15 +1,98 @@
 package winston.gameLibrary;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private CursorAdapter mCursorAdapter;
+    CursorAdapter adapter;
+    ListView listView;
+
+    ArrayList<Games> gamesDetail;
+
+    //fatboyslim menu
+    @Override
+    public boolean onCreateMenu (Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        //reference for searchable
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.searchView).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        return true;
+    }
+
+    //begin intents
+    @Override
+    protected void onNewIntent (Intent inten){
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent){
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.game_List_View);
+
+        //cursed cursors
+        Cursor cursor = GameSQLiteDatabaseHelper.getInstance(this).listGames();
+
+        for (cursor.moveToFirst();!cursor.isAfterLast();cursor.moveToNext()){
+            Games addGame = new Games();
+
+            addGame.setmName(cursor.getString(cursor.getColumnIndex("name")));
+            addGame.setmGenre(cursor.getString(cursor.getColumnIndex("genre")));
+            addGame.setmRelease(cursor.getString(cursor.getColumnIndex("release")));
+            addGame.setmBlurb(cursor.getString(cursor.getColumnIndex("blurb")));
+
+            gamesDetail.add(addGame);
+        }
+
+    }
+
+    //curse these cursoradapters
+    if (adapter == null){
+        adapter = new CursorAdapter(this, cursor, 1) {
+            @Override
+            public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
+                //fatboyslim xml
+                LayoutInflater layoutInflater = LayoutInflater.from(context);
+                View view = layoutInflater.inflate(R.layout.activity_games, viewGroup, false);
+                return view;
+            }
+
+            @Override
+            public void bindView(View view, Context context, Cursor cursor) {
+                TextView gameName = (TextView) view.findViewById(R.id.game_name);
+                String games = cursor.getString(cursor.getColumnIndex("name"));
+                gameName.setText(games);
+            }
+        };
+
+        listView.setAdapter(adapter);
+    }else{
+        //Swap in a new Cursor, returning the old Cursor.
+        adapter.swapCursor(cursor);
     }
 
 
